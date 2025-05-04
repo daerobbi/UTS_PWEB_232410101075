8e4e9e7c7e09cfa6dd0c9cbeeedee03ba66477a1
Dae Robbi <ahmadpratamadaerobbi@gmail.com>
2025-05-05 00:25:26 +0700

    'pp

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -14,12 +14,12 @@
         <p class="text-gray-700 mb-8 text-base md:text-lg">
             <span class="font-bold">AgriHive</span> adalah platform digital yang dirancang untuk membantu rekan tani dalam menerima pengajuan bibit dari agen secara cepat, menjangkau lebih banyak mitra baru, dan mengelola beragam katalog produk hortikultura. Lewat sistem terpadu, Anda bisa melihat broadcast kebutuhan agen, serta memastikan pasokan bibit selalu siap dan terdistribusi tepat sasaran.
         </p>
-        <button class="bg-yellow-400 hover:bg-yellow-500 text-black px-8 py-4 rounded-full flex items-center mx-auto lg:mx-0 space-x-2 text-lg">
+        <a href="{{ route('bibit.index') }}" class="bg-yellow-400 hover:bg-yellow-500 text-black w-auto py-4 rounded-full flex items-center justify-center space-x-2 text-lg">
             <span>Lihat Stok Bibit Anda</span>
-                <svg class="w-6 h-6" fill="none" stroke="currentColor" stroke-width="2" viewBox="0 0 24 24">
-                    <path stroke-linecap="round" stroke-linejoin="round" d="M9 5l7 7-7 7"/>
-                </svg>
-        </button>
+            <svg class="w-6 h-6" fill="none" stroke="currentColor" stroke-width="2" viewBox="0 0 24 24">
+                <path stroke-linecap="round" stroke-linejoin="round" d="M9 5l7 7-7 7"/>
+            </svg>
+        </a>
     </div>
 <div class="lg:w-1/2 flex justify-center">
     <img src="img/foto_orang.png" alt="Rekan Tani" class="w-full h-auto rounded-lg">
